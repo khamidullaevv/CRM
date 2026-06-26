e51e478ff8669f02470923921b6398235280f214
khamidullaevv <yusufbek090110@gmail.com>
2026-06-26 22:00:31 +0500

    im not back but i will try to create rice with QML

--- app/models.py
+++ app/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=122)
